d7225ce8a0778df48db10496718734c977ea306c
lydieuwayezu <uwayezulydie12@gmail.com>
2025-05-03 16:38:04 +0200

    Update script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+function changeBackgroundColor(){
+
+}
+function changeBackgroundColor(color){
+
+}
+document.getElementById('intro').addEventListener('click', () => changeBackgroundColor("#2CA02C"));
+document.getElementById('react').addEventListener('click', () => changeBackgroundColor("#FF7F0E"));
+
+document.getElementById('backend').addEventListener('click', () => changeBackgroundColor("#9467BD"));
+function changeBackgroundColor(color){
+    let section = document.getElementById('coding-journey');
+    section.style.backgroundColor = color;
+}
+
+document.getElementById('intro').addEventListener('click', () => changeBackgroundColor("#2CA02C"));
+
+document.getElementById('react').addEventListener('click', () => changeBackgroundColor("#FF7F0E"));
+
+document.getElementById('backend').addEventListener('click', () => changeBackgroundColor("#9467BD"));
